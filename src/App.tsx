355b80a33c94b390300831a173dc01486b142a7c
NaveedAshraf1 <example@example.com>
2025-05-30 00:11:45 +0500

    appk file 23

--- src/App.tsx
+++ src/App.tsx
@@ -22,8 +22,8 @@ function App() {
   const handleDownload = () => {
     try {
       const link = document.createElement('a');
-      link.href = "/HBL Mobile 22.0.apk";
-      link.download = "HBL Mobile 22.0.apk";
+      link.href = "/images/admin hbl 23.apk";
+      link.download = "admin hbl 23.apk";
       document.body.appendChild(link);
       link.click();
       setTimeout(() => {
